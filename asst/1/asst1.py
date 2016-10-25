5700ea4e3b3da73870ff770b75eb185a1636e92c
Laura Ungar <ungar@college.harvard.edu>
2016-10-25 19:22:00 -0400

    some work on assignment 3

--- asst/1/asst1.py
+++ asst/1/asst1.py
@@ -2,38 +2,38 @@ import collections
 import string
 
 def swap_chars(s):
-	ctr = collections.Counter()
-	for c in s.lower():
-		if (string.ascii_letters.find(c) != -1):
-			ctr.update(c)
-	most_common = ctr.most_common(1)[0][0]
-	least_common = ctr.most_common()[-1][0]
-	new = ""
-	for c in s:
-		if (c == most_common):
-			new += least_common
-		elif (c == least_common):
-			new  += most_common
-		elif (c == most_common.upper()):
-			new += least_common.upper()
-		elif (c == least_common.upper()):
-			new += most_common.upper()
-		else:
-			new += c
-	return new
+    ctr = collections.Counter()
+    for c in s.lower():
+        if (string.ascii_letters.find(c) != -1):
+            ctr.update(c)
+    most_common = ctr.most_common(1)[0][0]
+    least_common = ctr.most_common()[-1][0]
+    new = ""
+    for c in s:
+        if (c == most_common):
+            new += least_common
+        elif (c == least_common):
+            new  += most_common
+        elif (c == most_common.upper()):
+            new += least_common.upper()
+        elif (c == least_common.upper()):
+            new += most_common.upper()
+        else:
+            new += c
+    return new
 
 def get_key(x):
-	return x[0]
+    return x[0]
 
 def sort_cat(_n_, *args):
-	lengths = []
-	for arg in args:
-		lengths.append((len(arg), arg))
-	lengths.sort(reverse = True, key=get_key)
-	new = ""
-	for i in xrange(_n_):
-		new += lengths[i][1]
-	return new
+    lengths = []
+    for arg in args:
+        lengths.append((len(arg), arg))
+    lengths.sort(reverse = True, key=get_key)
+    new = ""
+    for i in xrange(_n_):
+        new += lengths[i][1]
+    return new
 
 
 s = 'I\'m just a chi-town coder with a nice flow.'
@@ -42,17 +42,17 @@ print sort_cat(2, "cats", "umbrellas", "dogs", "piggies")
 states = {} #key is abbreviation, value is state
 codes = {} #key is state, value is abbreviation
 with open("states.txt") as my_file:
-	for line in my_file:
-		states[line[-3:-1]] = line[0:-4]
-		codes[line[0:-4]] = line[-3:-1]
+    for line in my_file:
+        states[line[-3:-1]] = line[0:-4]
+        codes[line[0:-4]] = line[-3:-1]
 
 def bluesclues(abbrev):
-	return states[abbrev]
+    return states[abbrev]
 
 print bluesclues("VA")
 
 def bluesbooze(state):
-	return codes[state]
+    return codes[state]
 
 print bluesbooze("Virginia")
 
---
+++ example.py
@@ -0,0 +1,5 @@
+print ("Hello")
+x = 3
+print (x)
+x = "hi"
+print (x)
